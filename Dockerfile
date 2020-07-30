207ad913c16e9416db2ed1227b9f59263a93f631
Jonathan Williams <jpwilliams@mitre.org>
2020-07-30 17:59:51 -0500

    adding Ansible

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,9 @@ COPY aws-rds-crunchy-data-postgresql-9-stig-baseline/ /share/aws-rds-crunchy-dat
 COPY pgstigcheck-inspec/ /share/pgstigcheck-inspec/
 COPY inspec.sh /share/inspec.sh
 RUN yum install -y curl postgresql-client unzip
-RUN yum install -y python3 
+RUN yum install -y python3 epel-release
+RUN yum update -y
+RUN yum install -y ansible
 RUN curl 'https://packages.chef.io/files/stable/inspec/4.22.1/el/7/inspec-4.22.1-1.el7.x86_64.rpm' -o /tmp/inspec-4.22.1-1.el7.x86_64.rpm
 RUN yum localinstall --nogpgcheck -y /tmp/inspec-4.22.1-1.el7.x86_64.rpm
 RUN curl 'https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip' -o "awscliv2.zip" && unzip awscliv2.zip && ./aws/install
--- inspec.sh
+++ inspec.sh
@@ -9,33 +9,33 @@ echo "dont really know what this does"
 echo "getting ca bundle"
 wget https://s3.amazonaws.com/rds-downloads/rds-combined-ca-bundle.pem
 
-echo "install docker"
-dnf install docker
-echo "getting gpg and adding"
-curl -fsSL https://apt.releases.hashicorp.com/gpg | apt-key add -
-echo "add repo"
-apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-echo "update and install vault"
-apt-get update && sudo apt-get install vault
-
-
-
-#export RDSHOST="rdspostgres.abcdefghijk.us-west-2.rds.amazonaws.com"
-echo $RDSHOST
-echo "export pass"
-#export PGHOST = $RDSHOST
-export PGPASSWORD="$(aws rds generate-db-auth-token --hostname=$RDSHOST --port=$PGPORT --username=$PGUSER --region=$REGION)"
-echo "pwd"
-pwd
-echo "password:"
-echo $PGPASSWORD
-
-echo "psql with sslcert"
-echo 'psql -h $PGHOST -p $PGPORT "dbname=$PGDATABASE user=$PGUSER sslrootcert=/share/rds-combined-ca-bundle.pem sslmode=verify-ca"'
-psql -h $PGHOST -p $PGPORT "dbname=$PGDATABASE user=$PGUSER sslrootcert=/share/rds-combined-ca-bundle.pem sslmode=verify-ca"
-echo "ls"
-ls
-#inspec exec /share/aws-rds-crunchy-data-postgresql-9-stig-baseline --attrs /share/aws-rds-crunchy-data-postgresql-9-stig-baseline/attributes.yaml --reporter cli json:/share/output/$OUTFILE
+#echo "install docker"
+#dnf install docker
+#echo "getting gpg and adding"
+#curl -fsSL https://apt.releases.hashicorp.com/gpg | apt-key add -
+#echo "add repo"
+#apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+#echo "update and install vault"
+#apt-get update && sudo apt-get install vault
+#
+#
+#
+##export RDSHOST="rdspostgres.abcdefghijk.us-west-2.rds.amazonaws.com"
+#echo $RDSHOST
+#echo "export pass"
+##export PGHOST = $RDSHOST
+#export PGPASSWORD="$(aws rds generate-db-auth-token --hostname=$RDSHOST --port=$PGPORT --username=$PGUSER --region=$REGION)"
+#echo "pwd"
+#pwd
+#echo "password:"
+#echo $PGPASSWORD
+#
+#echo "psql with sslcert"
+#echo 'psql -h $PGHOST -p $PGPORT "dbname=$PGDATABASE user=$PGUSER sslrootcert=/share/rds-combined-ca-bundle.pem sslmode=verify-ca"'
+#psql -h $PGHOST -p $PGPORT "dbname=$PGDATABASE user=$PGUSER sslrootcert=/share/rds-combined-ca-bundle.pem sslmode=verify-ca"
+#echo "ls"
+#ls
+inspec exec /share/aws-rds-crunchy-data-postgresql-9-stig-baseline --attrs /share/aws-rds-crunchy-data-postgresql-9-stig-baseline/attributes.yaml --reporter cli json:/share/output/$OUTFILE
 echo "vault"
 vault --version
 echo "aws s3 upload"
